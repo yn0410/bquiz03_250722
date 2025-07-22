f8eb0f5e9c2d676ccc376d69bee27289af9ff30b
yn <jenny783113@gmail.com>
2025-07-22 14:24:14 +0800

    (加註解)

--- api/edit_poster.php
+++ api/edit_poster.php
@@ -1,9 +1,9 @@
 <?php include_once "db.php";
 
 foreach($_POST['id'] as $idx => $id){
-    if(isset($_POST['del']) && in_array($id, $_POST['del'])){
+    if(isset($_POST['del']) && in_array($id, $_POST['del'])){ //先看是否要刪除
         $Poster->del($id);
-    }else{
+    }else{ //沒有要刪除，再看要不要編輯
         $poster=$Poster->find($id);
         $poster['name']=$_POST['name'][$idx];
         $poster['sh']=(isset($_POST['sh']) && in_array($id, $_POST['sh']))?1:0;
