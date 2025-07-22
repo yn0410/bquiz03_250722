12837446200ad523725e94afa82a43410292e358
yn <jenny783113@gmail.com>
2025-07-22 10:37:01 +0800

    建立管理系統登入功能

--- back/main.php
+++ back/main.php
@@ -1,4 +1,58 @@
-    <div class="ct a rb" style="position:relative; width:101.5%; left:-1%; padding:3px; top:-9px;"> <a href="?do=admin&redo=tit">網站標題管理</a>| <a href="?do=admin&redo=go">動態文字管理</a>| <a href="?do=admin&redo=rr">預告片海報管理</a>| <a href="?do=admin&redo=vv">院線片管理</a>| <a href="?do=admin&redo=order">電影訂票管理</a> </div>
-    <div class="rb tab">
-      <h2 class="ct">請選擇所需功能</h2>
+<!--   if(登入成功){
+    <div class="ct a rb" style="position:relative; width:101.5%; left:-1%; padding:3px; top:-9px;">
+        <a href="?do=admin&redo=tit">網站標題管理</a>| 
+        <a href="?do=admin&redo=go">動態文字管理</a>| 
+        <a href="?do=admin&redo=rr">預告片海報管理</a>| 
+        <a href="?do=admin&redo=vv">院線片管理</a>| 
+        <a href="?do=admin&redo=order">電影訂票管理</a>
+      </div>
+  
+      <div class="rb tab">
+        <h2 class="ct">請選擇所需功能</h2>
+      </div>
+  }else{
+    login畫面
+  } -->
+
+<?php 
+if(isset($_POST['acc'])){
+    if($_POST['acc']=='admin' && $_POST['pw']=='1234'){
+        $_SESSION['admin']=$_POST['acc'];
+    }else{
+        echo "<div class='ct' style='color:red;'>帳號或密碼錯誤</div>";
+    }
+}
+?>
+
+
+
+<?php if(isset($_SESSION['admin'])): ?>
+<div class="ct a rb" style="position:relative; width:101.5%; left:-1%; padding:3px; top:-9px;">
+    <a href="?do=admin&redo=tit">網站標題管理</a>|
+    <a href="?do=admin&redo=go">動態文字管理</a>|
+    <a href="?do=admin&redo=rr">預告片海報管理</a>|
+    <a href="?do=admin&redo=vv">院線片管理</a>|
+    <a href="?do=admin&redo=order">電影訂票管理</a>
+</div>
+
+<div class="rb tab">
+    <h2 class="ct">請選擇所需功能</h2>
+</div>
+<?php else: ?>
+<form action="?" method="post">
+    <table style="width:300px; margin: auto;">
+        <tr>
+            <td>帳號：</td>
+            <td><input type="text" name="acc" id="acc"></td>
+        </tr>
+        <tr>
+            <td>帳號：</td>
+            <td><input type="password" name="pw" id="pw"></td>
+        </tr>
+    </table>
+    <div class="ct">
+        <input type="submit" value="登入">
+        <input type="reset" value="重置">
     </div>
+</form>
+<?php endif; ?>
