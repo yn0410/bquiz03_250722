fd1eba1346ac64cc77bb9b4628b2eea856914393
yn <jenny783113@gmail.com>
2025-07-22 11:14:23 +0800

    改沒注意到的地方

--- back.php
+++ back.php
@@ -38,11 +38,11 @@
 
     <?php if(isset($_SESSION['admin'])): ?>
         <div class="ct a rb" style="position:relative; width:101.5%; left:-1%; padding:3px; top:-9px;">
-            <a href="?do=admin&redo=title">網站標題管理</a>|
-            <a href="?do=admin&redo=ad">動態文字管理</a>|
-            <a href="?do=admin&redo=poster">預告片海報管理</a>|
-            <a href="?do=admin&redo=movie">院線片管理</a>|
-            <a href="?do=admin&redo=order">電影訂票管理</a>
+            <a href="?do=title">網站標題管理</a>|
+            <a href="?do=ad">動態文字管理</a>|
+            <a href="?do=poster">預告片海報管理</a>|
+            <a href="?do=movie">院線片管理</a>|
+            <a href="?do=order">電影訂票管理</a>
         </div>
         <?php
         $do=$_GET['do']??'main';
@@ -71,7 +71,7 @@
             </div>
         </form>
     <?php endif; ?>
-    
+
   </div>
   <div id="bo"> ©Copyright 2010~2014 ABC影城 版權所有 </div>
 </div>
