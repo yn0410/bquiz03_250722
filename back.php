82edc0ace065976b29ee6f800724a84985122ea3
yn <jenny783113@gmail.com>
2025-07-22 10:54:51 +0800

    修改前後台頁面內的連結及建立對應的檔案

--- back.php
+++ back.php
@@ -26,15 +26,52 @@
     </marquee>
   </div>
   <div id="mm">
-    <?php
-    $do=$_GET['do']??'main';
-    $file="./back/".$do.'.php';
-    if(file_exists($file)){
-      include $file;
-    }else{
-      include "./back/main.php";
+    <?php 
+    if(isset($_POST['acc'])){
+        if($_POST['acc']=='admin' && $_POST['pw']=='1234'){
+            $_SESSION['admin']=$_POST['acc'];
+        }else{
+            echo "<div class='ct' style='color:red;'>帳號或密碼錯誤</div>";
+        }
     }
     ?>
+
+    <?php if(isset($_SESSION['admin'])): ?>
+        <div class="ct a rb" style="position:relative; width:101.5%; left:-1%; padding:3px; top:-9px;">
+            <a href="?do=admin&redo=title">網站標題管理</a>|
+            <a href="?do=admin&redo=ad">動態文字管理</a>|
+            <a href="?do=admin&redo=poster">預告片海報管理</a>|
+            <a href="?do=admin&redo=movie">院線片管理</a>|
+            <a href="?do=admin&redo=order">電影訂票管理</a>
+        </div>
+        <?php
+        $do=$_GET['do']??'main';
+        $file="./back/".$do.'.php';
+        if(file_exists($file)){
+          include $file;
+        }else{
+          include "./back/main.php";
+        }
+        ?>
+    <?php else: ?>
+        <form action="?" method="post">
+            <table style="width:300px; margin: auto;">
+                <tr>
+                    <td>帳號：</td>
+                    <td><input type="text" name="acc" id="acc"></td>
+                </tr>
+                <tr>
+                    <td>密碼：</td>
+                    <td><input type="password" name="pw" id="pw"></td>
+                </tr>
+            </table>
+            <div class="ct">
+                <input type="submit" value="登入">
+                <input type="reset" value="重置">
+            </div>
+        </form>
+    <?php endif; ?>
+    
   </div>
   <div id="bo"> ©Copyright 2010~2014 ABC影城 版權所有 </div>
 </div>
