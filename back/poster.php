7c548de455efa72cd33261918a2c00a20bcd1f24
yn <jenny783113@gmail.com>
2025-07-22 13:51:08 +0800

    fix

--- back/poster.php
+++ back/poster.php
@@ -1,19 +1,19 @@
 <div style="height: 300px;">
     <h3 class="ct" style="margin: 0;">預告片清單</h3>
     <div class="ct" style="display: flex; justify-content: space-between;">
-        <div style="width:24.8%; background-color: #ccc;">預告片海報</div>
-        <div style="width:24.8%; background-color: #ccc;">預告片片名</div>
-        <div style="width:24.8%; background-color: #ccc;">預告片排序</div>
-        <div style="width:24.8%; background-color: #ccc;">操作</div>
+        <div style="width:24.8%; background:#ccc;">預告片海報</div>
+        <div style="width:24.8%; background:#ccc;">預告片片名</div>
+        <div style="width:24.8%; background:#ccc;">預告片排序</div>
+        <div style="width:24.8%; background:#ccc;">操作</div>
     </div>
-    <div style="overflow: auto; height:200px;">
+    <div style="overflow:auto; height:200px;">
         <?php
         $posters=$Poster->all(" order by `rank`");
         foreach($posters as $poster):
         ?>
-        <div class="ct" style="display: flex; justify-content: space-between; background: white; margin-bottom:3px;">
+        <div class="ct" style="display:flex; justify-content:space-between; background:white; margin-bottom:3px;">
             <div style="width:24.8%;">
-                <img src="../image/<?=$poster['img'];?>" style="width:60px; height:80px;">
+                <img src="./image/<?=$poster['img'];?>" style="width:60px; height:80px;">
             </div>
             <div style="width:24.8%;">
                 <input type="text" name="name[]" value="<?=$poster['name'];?>" style="width:90%;">
