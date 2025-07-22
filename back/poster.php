e5570276eea6949bb8b446e542265c10cb68c2d0
yn <jenny783113@gmail.com>
2025-07-22 11:34:18 +0800

    建立新增預告片表單及預告片資料表

--- back/poster.php
+++ back/poster.php
@@ -1,1 +1,23 @@
-預告片海報管理
+<div style="height: 300px;">
+    <h3 class="ct" style="margin: 0;">預告片清單</h3>
+</div>
+
+<hr>
+
+<div style="height: 140px;">
+<h3 class="ct" style="margin: 0;">新增預告片海報</h3>
+<form action="./api/add_poster.php" method="post">
+    <table>
+        <tr>
+            <td>預告片海報</td>
+            <td><input type="file" name="img" id="img"></td>
+            <td>預告片片名：</td>
+            <td><input type="text" name="name" id="name"></td>
+        </tr>
+    </table>
+    <div class="ct">
+        <input type="submit" value="新增">
+        <input type="reset" value="重置">
+    </div>
+</form>
+</div>
