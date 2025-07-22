6cfb475c464d120adfc47b2217a1926592929155
yn <jenny783113@gmail.com>
2025-07-22 09:40:45 +0800

    切版

--- index.php
+++ index.php
@@ -21,28 +21,15 @@
     </marquee>
   </div>
   <div id="mm">
-    <div class="half" style="vertical-align:top;">
-      <h1>預告片介紹</h1>
-      <div class="rb tab" style="width:95%;">
-        <div id="abgne-block-20111227">
-          <ul class="lists">
-          </ul>
-          <ul class="controls">
-          </ul>
-        </div>
-      </div>
-    </div>
-    <div class="half">
-      <h1>院線片清單</h1>
-      <div class="rb tab" style="width:95%;">
-        <table>
-          <tbody>
-            <tr> </tr>
-          </tbody>
-        </table>
-        <div class="ct"> </div>
-      </div>
-    </div>
+    <?php
+    $do=$_GET['do']??'main';
+    $file="./front/".$do.'.php';
+    if(file_exists($file)){
+      include $file;
+    }else{
+      include "./front/main.php";
+    }
+    ?>
   </div>
   <div id="bo"> ©Copyright 2010~2014 ABC影城 版權所有 </div>
 </div>
