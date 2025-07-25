99b4dab9773834359a9439d48a160532dfba744e
yn <jenny783113@gmail.com>
2025-07-25 13:34:31 +0800

    建立院線片後台的版型

--- back/movie.php
+++ back/movie.php
@@ -1,1 +1,31 @@
-院線片管理
+<button>新增電影</button>
+<hr>
+<style>
+    .movie{
+        display: flex;
+        width: 95%;
+        margin: auto;
+        box-shadow: 0 0 3px #999;
+    }
+</style>
+<div class="movie">
+    <div>
+        <img src="" alt="">
+    </div>
+    <div>
+        分級
+    </div>
+    <div>
+        <div></div>
+        <div>
+            <button>顯示</button>
+            <button>往上</button>
+            <button>往下</button>
+            <button>編輯電影</button>
+            <button>刪除電影</button>
+        </div>
+        <div>
+            劇情介紹
+        </div>
+    </div>
+</div>
