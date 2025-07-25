a4bac14390985b166147b1f3e8563ef410b555f5
yn <jenny783113@gmail.com>
2025-07-25 15:40:38 +0800

    修改院線片管理的版型 &新增院線片3~13至DB中

--- back/movie.php
+++ back/movie.php
@@ -9,6 +9,7 @@
         box-shadow: 0 0 3px #999;
         align-items: center;
         padding: 2px;
+        margin-bottom: 5px;
     }
 
     .movie > div:nth-child(1){ /* 縮圖(?) */
@@ -33,34 +34,36 @@
         width: 33%;
     }
 </style>
-<?php
-$movies = $Movie->all(" order by `rank`");
-foreach($movies as $movie):
-?>
-<div class="movie">
-    <div>
-        <img src="./image/<?=$movie['poster']?>" style="width:60px; height:80px;">
-    </div>
-    <div>
-        分級: <img src="./icon/03C0<?=$movie['level']?>.png" style="width:20px;">
-    </div>
-    <div>
+<div style="height:450px; overflow:auto;">
+    <?php
+    $movies = $Movie->all(" order by `rank`");
+    foreach($movies as $movie):
+    ?>
+    <div class="movie">
         <div>
-            <div>片名：<?=$movie['name'];?></div>
-            <div>片長：<?=$movie['length'];?></div>
-            <div>上映時間：<?=$movie['ondate'];?></div>
+            <img src="./image/<?=$movie['poster']?>" style="width:60px; height:80px;">
         </div>
         <div>
-            <button>顯示</button>
-            <button>往上</button>
-            <button>往下</button>
-            <button>編輯電影</button>
-            <button>刪除電影</button>
+            分級: <img src="./icon/03C0<?=$movie['level']?>.png" style="width:20px;">
         </div>
         <div>
-            劇情介紹：<?=$movie['intro'];?>
+            <div>
+                <div>片名：<?=$movie['name'];?></div>
+                <div>片長：<?=$movie['length'];?></div>
+                <div>上映時間：<?=$movie['ondate'];?></div>
+            </div>
+            <div>
+                <button>顯示</button>
+                <button>往上</button>
+                <button>往下</button>
+                <button>編輯電影</button>
+                <button>刪除電影</button>
+            </div>
+            <div>
+                劇情介紹：<?=$movie['intro'];?>
+            </div>
         </div>
     </div>
+    <hr>
+    <?php endforeach; ?>
 </div>
-<hr>
-<?php endforeach; ?>
