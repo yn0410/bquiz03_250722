bf574b1d81709fdfc8867e42508e9bc3cb63fb81
yn <jenny783113@gmail.com>
2025-07-25 13:27:27 +0800

    更新 院線片資料表

--- db07_03.sql
+++ db07_03.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- 主機： 127.0.0.1
--- 產生時間： 2025-07-22 05:31:27
+-- 產生時間： 2025-07-25 07:25:58
 -- 伺服器版本： 10.4.32-MariaDB
 -- PHP 版本： 8.2.12
 
@@ -24,6 +24,27 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- 資料表結構 `movies`
+--
+
+CREATE TABLE `movies` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `name` text NOT NULL COMMENT '電影片名',
+  `level` int(1) UNSIGNED NOT NULL COMMENT '分級',
+  `length` int(10) UNSIGNED NOT NULL COMMENT '時長',
+  `ondate` date NOT NULL COMMENT '上映日期',
+  `publish` text NOT NULL COMMENT '發行商',
+  `director` text NOT NULL COMMENT '導演',
+  `trailer` text NOT NULL COMMENT '預告影片',
+  `poster` text NOT NULL COMMENT '電影海報',
+  `intro` text NOT NULL COMMENT '劇情簡介',
+  `sh` int(1) NOT NULL COMMENT '顯示?',
+  `rank` int(10) UNSIGNED NOT NULL COMMENT '排序'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
 -- 資料表結構 `posters`
 --
 
@@ -37,8 +58,26 @@ CREATE TABLE `posters` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 --
+-- 傾印資料表的資料 `posters`
+--
+
+INSERT INTO `posters` (`id`, `name`, `img`, `sh`, `rank`, `ani`) VALUES
+(1, '預告片01', '03A01.jpg', 1, 1, 2),
+(3, '預告片三', '03A03.jpg', 1, 3, 1),
+(4, '預告片二', '03A02.jpg', 1, 4, 3),
+(5, '預告片四', '03A04.jpg', 1, 5, 2),
+(6, '預告片五', '03A05.jpg', 1, 6, 1),
+(7, '預告片六', '03A06.jpg', 1, 7, 3);
+
+--
 -- 已傾印資料表的索引
 --
+
+--
+-- 資料表索引 `movies`
+--
+ALTER TABLE `movies`
+  ADD PRIMARY KEY (`id`);
 
 --
 -- 資料表索引 `posters`
@@ -51,10 +90,16 @@ ALTER TABLE `posters`
 --
 
 --
+-- 使用資料表自動遞增(AUTO_INCREMENT) `movies`
+--
+ALTER TABLE `movies`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
 -- 使用資料表自動遞增(AUTO_INCREMENT) `posters`
 --
 ALTER TABLE `posters`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
