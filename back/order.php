a63c67f72cf1f00dfdf807dec40f4f4be51845db
yn <jenny783113@gmail.com>
2025-08-01 15:19:12 +0800

    建立 後台-訂單管理的快速刪除功能區 畫面

--- back/order.php
+++ back/order.php
@@ -1,1 +1,17 @@
-電影訂票管理
+<h3 class="ct">訂單管理</h3>
+<div class="quick-del">
+    快速刪除：
+    <!-- input:radio[name='type'] -->
+     <input type="radio" name="type" id="date" value="date" checked>依日期
+     <input type="text" name="date">
+     <input type="radio" name="type" id="movie" value="movie">依電影
+     <select name="movie"> <!-- 訂單裡有被訂的電影 -->
+        <?php
+        $movies = q("select `movie` from `orders` group by `movie`");
+        foreach($movies as $movie){
+            echo "<option value='{$movie['movie']}'>{$movie['movie']}</option>";
+        }
+        ?>
+
+     </select>
+</div>
