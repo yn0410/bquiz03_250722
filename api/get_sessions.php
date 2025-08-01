a6570fa5e8c1fcbddeb44f57fd5a88d093865771
yn <jenny783113@gmail.com>
2025-08-01 16:16:10 +0800

    完成 訂票選單的剩餘座位功能

--- api/get_sessions.php
+++ api/get_sessions.php
@@ -2,7 +2,7 @@
 
 $id=$_GET['movieId'];
 $date=$_GET['date'];
-$movies = $Movie->find($id);
+$movie = $Movie->find($id);
 /* $ondate = strtotime($movies['ondate']); //??
 $today = strtotime(date("Y-m-d"));//今日 */
 
@@ -14,7 +14,7 @@ if($date==date("Y-m-d") && $hr>13){
 
 
 for($i=$start; $i<5 ;$i++){
-    $remaining=20; //假設每場剩餘20個座位, (20-已被訂走的座位數)
+    $remaining=20-$Order->sum('tickets',['movie'=>$movie['name'], 'date'=>$date, 'session'=>$sessStr[$i]]); //假設每場剩餘20個座位, (20-已被訂走的座位數)
     echo "<option value='{$sessStr[$i]}'>";
     echo $sessStr[$i];
     echo " 剩餘座位 ";
